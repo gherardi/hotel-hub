583d315915e72effd771b4bf98aff7da2ab082ea
Victor Gherardi <gherardivictor@gmail.com>
2024-02-06 22:14:17 +0100

    refactor(auth): splitting components [solid]

--- client/src/components/AuthProvider.jsx
+++ client/src/components/AuthProvider.jsx
@@ -1,21 +1,16 @@
 import { createContext, useContext, useState } from 'react';
-import useApi from '../hooks/useApi.jsx';
-import ServerErrorPage from '../pages/ServerErrorPage.jsx';
-import LoadingPage from '../pages/LoadingPage.jsx';
+import ServerStatusChecker from './ServerStatusChecker';
 
 const AuthContext = createContext(null);
 
 export default function AuthProvider({ children, isSignedIn }) {
-	const { isLoading, error } = useApi('https://jsonplaceholder.typicode.com/posts/1');
-	// const { isLoading, error } = useApi('/server-status');
-
 	const [user] = useState(isSignedIn ? { id: 1, username: 'user' } : null);
 
-	if (error) return <ServerErrorPage error={error} />;
-
-	if (isLoading) return <LoadingPage />;
-
-	return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+	return (
+		<ServerStatusChecker>
+			<AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+		</ServerStatusChecker>
+	);
 }
 
 export const useAuth = () => {
