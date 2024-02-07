e198cfaa821edc8f7526c6ed42471522ed2777b0
Victor Gherardi <gherardivictor@gmail.com>
2024-02-07 18:56:43 +0100

    style(css): improved the error page just a little bit

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,3 +1,7 @@
 export default function ErrorPage({ error }) {
-	return <div>{error ? error : 'something went very wrong...'}</div>;
+	return (
+		<div className='flex items-center justify-center h-svh'>
+			<div>{error ? error : 'something went very wrong...'}</div>;
+		</div>
+	);
 }
