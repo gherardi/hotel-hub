1bc0ba270b327a4d537a7601a18186d2554bdb66
Victor Gherardi <gherardivictor@gmail.com>
2024-02-18 21:49:48 +0100

    refactor(auth): authentication better

--- client/src/components/AuthProvider.jsx
+++ client/src/components/AuthProvider.jsx
@@ -1,23 +1,24 @@
-import { createContext, useContext, useState } from 'react';
-import ServerStatusChecker from './ServerStatusChecker';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext(null);
 
-export default function AuthProvider({ children, isSignedIn }) {
-	const [user] = useState(isSignedIn ? { id: 1, username: 'user' } : null);
+export default function AuthProvider({ children }) {
+	// const [user] = useState(isSignedIn ? { id: 1, username: 'user' } : null);
 
-	return (
-		<ServerStatusChecker>
-			<AuthContext.Provider value={user}>{children}</AuthContext.Provider>
-		</ServerStatusChecker>
-	);
+	// const user = { uuid: crypto.randomUUID() };
+	const [user] = useState({ uuid: '1234' });
+
+	useEffect(() => {}, []);
+
+	return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 }
 
 export const useAuth = () => {
 	const context = useContext(AuthContext);
 
-	if (context === undefined) {
+	if (context === null) {
 		throw new Error('useAuth must be used within an AuthProvider');
 	}
+
 	return context;
 };
