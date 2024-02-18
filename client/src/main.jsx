00a6bdf02d9c324714ae9fc83325962677171b30
Victor Gherardi <gherardivictor@gmail.com>
2024-02-18 21:50:13 +0100

    feat(lazy): lazy loading pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,53 +1,70 @@
 import React from 'react';
+import { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import AuthProvider from './components/AuthProvider.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 
-import HomePage from './pages/HomePage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import SignUpPage from './pages/SignUpPage.jsx';
+import LoadingPage from './pages/Loading.page.jsx';
 
-import AuthProvider from './components/AuthProvider.jsx';
-import ProtectedPage from './pages/ProtectedPage.jsx';
+const LoginPage = lazy(() => import('./pages/Login.page.jsx'));
+const HomePage = lazy(() => import('./pages/Home.page.jsx'));
+const SignUpPage = lazy(() => import('./pages/Signup.page.jsx'));
+const DashboardPage = lazy(() => import('./pages/Dashboard.page.jsx'));
 
-import NotFoundPage from './pages/NotFoundPage.jsx';
-
-// import { lazy } from "react";
-// const ReportedPost = lazy(() => import("./pages/ReportedPost"));
+const NotFoundPage = lazy(() => import('./pages/NotFound.page.jsx'));
 
 // complete react router tutorial by cosden solutions
 // https://youtu.be/oTIJunBa6MA?feature=shared
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <HomePage />,
+		element: (
+			<Suspense fallback={<LoadingPage />}>
+				<HomePage />
+			</Suspense>
+		),
 	},
 	{
 		path: '/login',
-		element: <LoginPage />,
+		element: (
+			<Suspense fallback={<LoadingPage />}>
+				<LoginPage />
+			</Suspense>
+		),
 	},
 	{
 		path: '/signup',
-		element: <SignUpPage />,
+		element: (
+			<Suspense fallback={<LoadingPage />}>
+				<SignUpPage />
+			</Suspense>
+		),
 	},
 	{
-		path: '/protect',
+		path: '/dashboard',
 		element: (
-			<ProtectedRoute>
-				<ProtectedPage />
-			</ProtectedRoute>
+			<Suspense fallback={<LoadingPage />}>
+				<ProtectedRoute>
+					<DashboardPage />
+				</ProtectedRoute>
+			</Suspense>
 		),
 	},
 	{
 		path: '*',
-		element: <NotFoundPage />,
+		element: (
+			<Suspense fallback={<LoadingPage />}>
+				<NotFoundPage />
+			</Suspense>
+		),
 	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<AuthProvider isSignedIn={false}>
+		<AuthProvider>
 			<RouterProvider router={router} />
 		</AuthProvider>
 	</React.StrictMode>
