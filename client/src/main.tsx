088e86aabaa9ee6d81a993c1e9b3731405615b54
Victor <gherardivictor@gmail.com>
2024-06-04 22:19:20 +0200

    react query devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { ThemeProvider } from '@/components/theme-provider.tsx';
 import AuthProvider from '@/components/auth-provider.tsx';
@@ -19,6 +20,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 					</BrowserRouter>
 				</AuthProvider>
 			</ThemeProvider>
+			{/* QUERY CLIENT DEV TOOLS */}
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</React.StrictMode>
 );
